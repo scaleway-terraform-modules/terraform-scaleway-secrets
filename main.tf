04e050e1312936d028b969748a0b0def2682404d
Benoit Garcia <benoit.garcia@protonmail.com>
2025-11-25 00:47:28 +0100

    chore: reorder attributes

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ resource "scaleway_secret" "this" {
   name        = var.name
   description = var.description
   region      = var.region
+  path        = var.path
   project_id  = var.project_id
   tags        = var.tags
-  path        = var.path
 }
 
 resource "scaleway_secret_version" "this" {
+  secret_id   = scaleway_secret.this.id
   data        = var.data
   description = var.description
   region      = var.region
-  secret_id   = scaleway_secret.this.id
 }
